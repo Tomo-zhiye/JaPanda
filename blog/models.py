3bbc40b53988018fff5f81d28e1ae8de8074a875
Your Name <you@example.com>
2021-05-04 15:22:15 +0900

    ajax-search

--- blog/models.py
+++ blog/models.py
@@ -6,58 +6,66 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 def user_directory_path(instance, filename):
-    return 'posts/{0}/{1}'.format(instance.id, filename)
+    return 'posts/%Y/%m/%d/'.format(instance.id, filename)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
 
 class Post(models.Model):
+
     class NewManager(models.Manager):
         def get_queryset(self):
-            return super().get_queryset().filter(status='published')
+            return super().get_queryset() .filter(status='published')
 
     options = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
     title = models.CharField(max_length=250)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     excerpt = models.TextField(null=True)
-    image = models.ImageField(upload_to='posts/', default='posts/default.jpg')
-    slug = models.CharField(max_length=250)
-    publish_date = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    image = models.ImageField(
+        upload_to=user_directory_path, default='posts/default.jpg')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     status = models.CharField(max_length=10, choices=options, default='draft')
-    objects = models.Manager()
-    new_manager = NewManager()
+    objects = models.Manager()  # default manager
+    newmanager = NewManager()  # custom manager
 
     def get_absolute_url(self):
         return reverse('blog:post_single', args=[self.slug])
 
     class Meta:
-        ordering = ('-publish_date',)
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
 
+
 class Comment(MPTTModel):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=50)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE,
+                            null=True, blank=True, related_name='children')
     email = models.EmailField()
     content = models.TextField()
-    publish_date = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
 
     class MPTTMeta:
-        order_insertion_by = ['publish_date',]
+        order_insertion_by = ['publish']
 
     def __str__(self):
-        return f'Comment by {self.name}'
+        return self.name
